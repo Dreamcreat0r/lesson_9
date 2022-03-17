558550d2d9628bd1effec7da737d44245c175cd9
Alexander Davydov <dreamcreat0r@outlook.com>
2022-03-17 16:30:10 +0300

    2 задача выполнена, есть нюанс - т.к. файл записи все время открыт, нельзя будет в середине процесса скопировать его и посмотреть результаты

--- lesson_9/02_log_parser.py
+++ lesson_9/02_log_parser.py
@@ -20,9 +20,60 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
 # TODO здесь ваш код
+class Parser:
+    def __init__(self, events_file, combine_by = 'by minutes'):
+
+        self.events = events_file
+        self.comb = combine_by
+        self.nok_count = 0
+        #self.combine_edges = []
+
+    def combiner(self):
+
+        if self.comb == 'by minutes':
+            self.right_edge = 17
+        elif self.comb == 'by hours':
+            self.right_edge = 14
+        elif self.comb == 'by days':
+            self.right_edge = 11
+        elif self.comb == 'by months':
+            self.right_edge = 8
+        elif self.comb == 'by years':
+            self.right_edge = 5
+
+    def engine(self, stat_file):
+
+        self.combiner()
+        left = 0
+        right = self.right_edge
+        prev_str = 'start'
+
+        events = open(self.events, 'r', encoding='cp1251')
+        stats = open(stat_file, 'w', encoding='utf8')
+        for string in events:
+            if string.endswith('NOK\n'):
+                if string[left:right] == prev_str:
+                    self.nok_count += 1
+                else:
+                    if self.nok_count:
+                        stats.write(f'{prev_str}] {self.nok_count}\n')
+                    self.nok_count = 1  # нада чета думатьadws
+                    prev_str = string[left:right]
+        stats.write(f'{prev_str}] {self.nok_count}\n')
+        events.close()
+        stats.close()
+
+fp = Parser('C:\\Users\\Alex\\source\\repos\\lesson_9\\lesson_9\\events.txt', 'by hours')
+fp.engine('C:\\Users\\Alex\\source\\repos\\lesson_9\\lesson_9\\out.txt')
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
 #  - по месяцу
 #  - по году
 # Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
+
+
+
+
+
+# задача выполнена, есть нюанс - т.к. файл записи все время открыт, нельзя будет в середине процесса скопировать его и посмотреть результаты
